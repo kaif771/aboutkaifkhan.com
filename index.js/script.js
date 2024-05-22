7ba9b6de50af3fd2cd6f74ea2c1cc04d2a4ed811
kaif771 <144323555+kaif771@users.noreply.github.com>
2024-05-22 07:13:41 +0000

    Add files via upload

---
+++ index.js/script.js
@@ -0,0 +1,27 @@
+console.log("script running...")
+document.querySelector('.skillbutton').addEventListener("click", () => {
+    if (document.querySelector('.skillbutton').classList.contains('skillgo')) {
+        document.querySelector('.allminiabout').style.display = 'none';
+        document.querySelector('.miniskill').style.display = 'inline';
+
+
+
+    }
+})
+document.querySelector('.aboutsec2').addEventListener("click", () => {
+    if (document.querySelector('.allminiabout').classList.contains('miniaboutgo')) {
+        document.querySelector('.miniskill').style.display = 'none';
+        document.querySelector('.allminiabout').style.display = 'inline';
+
+    }
+})
+document.querySelector('.miniskill').style.display = 'none';
+
+document.querySelector('.hamburger').addEventListener('click',() =>{
+    document.querySelector('.navigation').style.height = '300px';
+    document.querySelector('.navlist').classList.toggle('v-class')
+   if(document.querySelector('.navigation').classList.toggle('navigationgo')){
+      document.querySelector('.navigation').style.height = '50px';
+   }
+
+ })
